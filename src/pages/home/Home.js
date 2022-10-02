9a2df5ee46355f0785cbcc866e710acec662df3a
tohbay <tobechukwuobitube@gmail.com>
2022-10-02 08:44:23 +0100

    implement home page desktop view

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -6,23 +6,28 @@ import { CreateEventButton, HomeContainer } from "./HomeStyles";
 const Home = () => {
   return (
     <HomeContainer>
-      <header>
-        <h1 className="text-center">
-          Imagine if <br />
-          <span>Snapchat</span> <br />
-          had events.
-        </h1>
-        <p className="text-center">
-          Easily host and share events with your friends across any social
-          media.
-        </p>
-      </header>
-      <section>
+      <div className="show-on-desktop">
         <img src={homeImage} alt="event photo" />
-      </section>
-      <section>
-        <CreateEventButton>🎉 Create my event</CreateEventButton>
-      </section>
+      </div>
+      <div>
+        <header>
+          <h1 className="text-center">
+            Imagine if <br />
+            <span>Snapchat</span> <br />
+            had events.
+          </h1>
+          <p className="text-center">
+            Easily host and share events with your friends across any social
+            media.
+          </p>
+        </header>
+        <section className="home-page-image">
+          <img src={homeImage} alt="event photo" />
+        </section>
+        <section className="create-event">
+          <CreateEventButton>🎉 Create my event</CreateEventButton>
+        </section>
+      </div>
     </HomeContainer>
   );
 };
--- src/pages/home/HomeStyles.js
+++ src/pages/home/HomeStyles.js
@@ -21,11 +21,24 @@ export const HomeContainer = styled.div`
       background-clip: text;
       text-fill-color: transparent;
     }
+
+    @media (min-width: 992px) {
+      text-align: right;
+    }
+  }
+
+  .home-page-image {
+    display: flex;
+    justify-content: center;
   }
 
   img {
     width: 10.352rem;
     height: 18.25rem;
+
+    @media (min-width: 992px) {
+      display: none;
+    }
   }
 
   p {
@@ -35,6 +48,23 @@ export const HomeContainer = styled.div`
     color: #4f4f4f;
     line-height: 1.15rem;
     margin-bottom: 2.25rem;
+
+    @media (min-width: 992px) {
+      text-align: right;
+    }
+  }
+
+  .create-event {
+    display: flex;
+    justify-content: center;
+  }
+
+  .show-on-desktop {
+    display: none;
+
+    img {
+      display: none;
+    }
   }
 
   @media (min-width: 576px) and (max-width: 992px) {
@@ -58,7 +88,39 @@ export const HomeContainer = styled.div`
     }
   }
 
-  @media and (min-width: 992px) {
+  @media (min-width: 992px) {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    height: 100vh;
+    width: 80vw;
+    margin: 0 auto;
+
+    h1 {
+      font-size: 4rem;
+      line-height: 4rem;
+    }
+
+    p {
+      width: 32.5625rem;
+      font-size: 1.5rem;
+      line-height: 1.725rem;
+    }
+
+    .show-on-desktop {
+      display: block;
+
+      img {
+        display: block;
+        height: 48.48rem;
+        width: 27.5rem;
+      }
+    }
+
+    .create-event {
+      display: flex;
+      justify-content: flex-end;
+    }
   }
 `;
 
@@ -75,6 +137,15 @@ export const CreateEventButton = styled.button`
 
   @media (min-width: 576px) and (max-width: 992px) {
     margin-top: 3.3rem;
+    width: 20rem;
+    height: 3.44rem;
+    font-size: 1.25rem;
+    line-height: 1.44rem;
+    margin-bottom: 1.5rem;
+  }
+
+  @media (min-width: 992px) {
+    margin-top: 3.25rem;
     width: 20rem;
     height: 3.44rem;
     font-size: 1.25rem;
